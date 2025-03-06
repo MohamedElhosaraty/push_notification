8ececce49e53569e5cb41cb4eb7964490c557c34
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2025-03-06 18:58:27 +0200

    add sound

--- android/app/src/main/kotlin/com/push_notifications/push_notifications/MainActivity.kt
+++ android/app/src/main/kotlin/com/push_notifications/push_notifications/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.push_notifications.push_notifications
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Notifications.createNotificationChannels(this) // استخدام السياق بشكل صحيح
+    }
+}
